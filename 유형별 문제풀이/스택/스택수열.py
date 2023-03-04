8005b5ee8f02eefd7db20f9d605251a8359df1ea
dlsrnjs <dlsrnjs125@naver.com>
2023-03-04 22:38:35 +0900

    수정

---
+++ 유형별 문제풀이/스택/스택수열.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+count = 1
+stack = []
+result = []
+
+for i in range(1, n+1):
+    data = int(input())
+    while count <= data:
+        stack.append(count)
+        count += 1
+        result.append('+')
+    if stack [-1]== data: # 스택 최상위값과 비교
+        stack.pop()
+        result.append('-')
+    else:
+        print('NO')
+        exit(0)
+
+print('\n'.join(result))
