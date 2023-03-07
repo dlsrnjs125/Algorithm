50ab5245a6e6ce5e2f6d72031313dca9da4405b5
dlsrnjs <dlsrnjs125@naver.com>
2023-03-07 21:11:39 +0900

    20230307_update

--- 유형별 문제풀이/배열/수찾기.py
+++ 유형별 문제풀이/배열/수찾기.py
@@ -1,8 +1,8 @@
 # 20230307
 n = int(input())
-n_data = set(map(int, input().split(' ')))
+n_data = set(map(int, input().split()))
 m = int(input())
-m_data = list(map(int, input().split(' ')))
+m_data = list(map(int, input().split()))
 
 for i in m_data:
     if i not in n_data:
