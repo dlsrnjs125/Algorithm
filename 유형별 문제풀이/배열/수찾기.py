4eac1628f68706c9ed8732985c26429bf83a3da7
dlsrnjs <dlsrnjs125@naver.com>
2023-03-07 20:47:35 +0900

    20230307_update

---
+++ 유형별 문제풀이/배열/수찾기.py
@@ -0,0 +1,11 @@
+# 20230307
+n = int(input())
+n_data = set(map(int, input().split(' ')))
+m = int(input())
+m_data = list(map(int, input().split(' ')))
+
+for i in m_data:
+    if i not in n_data:
+        print('0')
+    else:
+        print('1')
