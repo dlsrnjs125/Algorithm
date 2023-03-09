d4b777a3a43e6a7a28bc676cd7ef42bd3c857aca
dlsrnjs <dlsrnjs125@naver.com>
2023-03-09 14:43:08 +0900

    20230309_update

---
+++ 유형별 문제풀이/정렬/수정렬하기.py
@@ -0,0 +1,29 @@
+# 20230309
+# 선택정렬 알고리즘
+n = int(input())
+array = list()
+
+for _ in range(n):
+    array.append(int(input()))
+    
+for i in range(n):
+    min_index = i
+    for j in range(i, n):
+        if array[min_index] > array[j]:
+            min_index = j
+        array[i], array[min_index] = array[min_index], array[i]
+        
+for i in array:
+    print(i)
+    
+# 파이썬 기본 정렬 알고리즘 이용
+n = int(input())
+array = list()
+
+for _ in range(n):
+    array.append(int(input()))
+                 
+array.sort()
+
+for i in array:
+    print(i)
