eafaf3e6608a2bbc284e57eaf7660907d577913e
dlsrnjs <dlsrnjs125@naver.com>
2023-03-10 11:35:06 +0900

    20230310_update

---
+++ 유형별 문제풀이/정렬/좌표정렬하기.py
@@ -0,0 +1,13 @@
+# 20230310
+n = int(input())
+
+array = []
+
+for _ in range(n):
+    x, y = map(int, input().split(' '))
+    array.append((x, y))
+    
+array = sorted(array)
+
+for i in array:
+    print(i[0], i[1])
