4a3e31d385e1cb9df8af73f3872964e57ebaa31a
dlsrnjs <dlsrnjs125@naver.com>
2023-03-10 11:39:50 +0900

    20230310_update

--- 유형별 문제풀이/정렬/좌표정렬하기.py
+++ 유형별 문제풀이/정렬/좌표정렬하기.py
@@ -1,13 +1,13 @@
 # 20230310
 n = int(input())
 
-array = []
+array = [] # tuple 사용
 
 for _ in range(n):
     x, y = map(int, input().split(' '))
     array.append((x, y))
     
-array = sorted(array)
+array = sorted(array) # 기본라이브러리 사용 -> 오름차순으로 정렬
 
 for i in array:
     print(i[0], i[1])
