93771bc10fbff7b3bd271be486c8e006f3c72cb4
inkwon <dlsrnjs125@naver.com>
2023-04-12 19:52:24 +0900

    20230412_update

--- 유형별 문제풀이/그래프/DFS와BFS.py
+++ 유형별 문제풀이/그래프/DFS와BFS.py
@@ -3,13 +3,13 @@ from collections import deque
 
 def dfs(v):
     print(v, end=' ')
-    visited[v] = True
+    visited[v] = True # 방문한 원소 방문처리
     for e in adj[v]:
         if not(visited[e]):
             dfs(e)
 
 def bfs(v):
-    q = deque([v])
+    q = deque([v]) # bfs 사용시 필요
     while q:
         v = q.popleft()
         if not(visited[v]):
@@ -20,6 +20,7 @@ def bfs(v):
                     q.append(e)
 
 n, m, v = map(int, input().split())
+# 연결리스트 이용
 adj = [[] for _ in range(n + 1)]
 
 for _ in range(m): 
@@ -27,6 +28,7 @@ for _ in range(m):
     adj[x].append(y)
     adj[y].append(x)
 
+# 연결리스트 정렬 시켜줌으로서 가장 낮은 번호부터
 for e in adj:
     e.sort()
 
@@ -34,5 +36,4 @@ visited = [False] * (n + 1)
 dfs(v)
 print()
 visited = [False] * (n + 1)
-
 bfs(v)
