56603583fc3c69db7fdc100baa02cd4fc0f07de0
inkwon <dlsrnjs125@naver.com>
2023-04-13 14:35:34 +0900

    20230413_update

--- 유형별 문제풀이/그래프/효율적인해킹.py
+++ 유형별 문제풀이/그래프/효율적인해킹.py
@@ -3,30 +3,57 @@ from collections import deque
 
 n, m = map(int, input().split())
 adj = [[] for _ in range(n + 1)]
+count = 0
 
 for _ in range(m):
     x, y = map(int, input().split())
     adj[y].append(x)
 
 def bfs(v):
-    q = deque([v])
+    global count
+    count = 0
     visited = [False] * (n + 1)
+    q = deque([v])
     visited[v] = True
-    count = 1
     while q:
         v = q.popleft()
         for e in adj[v]:
             if not visited[e]:
                 q.append(e)
                 visited[e] = True
-                count += 1
+    count = visited.count(True)
     return count
+
+def dfs(v):
+    global count
+    count = 0
+    global visited
+    visited[v] = True
+    for e in adj[v]:
+        if not(visited[e]):
+            visited[e] = True
+            dfs(e)
+    count = visited.count(True)
+    return count
+
 
 result = []
 max_value = - 1
 
+# bfs 오름차순으로 출력
 for i in range(1, n + 1):
     c = bfs(i)
+    if c > max_value:
+        result = [i]
+        max_value = c
+    elif c == max_value:
+        result.append(i)
+        max_value = c
+        
+# dfs 오름차순으로 출렭
+for i in range(1, n + 1):
+    visited = [False] * (n + 1)
+    c = dfs(i)
     if c > max_value:
         result = [i]
         max_value = c
