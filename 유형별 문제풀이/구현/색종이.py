25ffc267eda2f9019ac5954fb45af7e3320f350d
inkwon <dlsrnjs125@naver.com>
2023-04-17 10:48:41 +0900

    20230417_update

--- 유형별 문제풀이/구현/색종이.py
+++ 유형별 문제풀이/구현/색종이.py
@@ -1,1 +1,15 @@
 # 20230415
+n = int(input())
+
+paper = [[0]* 101 for i in range(101)]
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    for i in range(10):
+        for j in range(10):
+            paper[x+i][y+j] = 1
+            
+result = 0
+for i in paper:
+    result += sum(i)
+print(result)
