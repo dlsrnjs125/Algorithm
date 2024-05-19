31424b8d3f1fdc78f60ae8e65e34eef83fc50710
inkwon <dlsrnjs125@naver.com>
2024-05-19 12:29:52 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -1,8 +1,7 @@
 def solution(array, height):
     answer = 0
-    tmp = sorted(array, reverse=True)
     
-    for i in tmp:
+    for i in array:
         if i > height:
             answer += 1
     return answer
