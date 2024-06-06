31db4d1074cfc9ac0ece50d5218247c3f77141c8
inkwon <dlsrnjs125@naver.com>
2024-06-06 22:42:26 +0900

    [level 0] Title: 배열의 유사도, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -2,7 +2,6 @@ def solution(s1, s2):
     answer = 0
     
     for i in s1:
-        for j in s2:
-            if i == j:
-                answer += 1
+        if i in s2:
+            answer += 1
     return answer
