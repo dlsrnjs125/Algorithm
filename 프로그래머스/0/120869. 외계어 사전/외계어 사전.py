2525873b2c6ddd23ae6a92a9ce8ec1ebad2aa38f
inkwon <dlsrnjs125@naver.com>
2024-06-18 22:50:09 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.py
@@ -1,8 +1,9 @@
 def solution(spell, dic):
     answer = 0
+    spell = sorted(spell)
     
     for i in dic:
-        if sorted(spell) == sorted(i):
+        if spell == sorted(i):
             answer = 1
             break
         else:
