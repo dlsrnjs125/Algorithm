bfa18fe7c4ec6f4029a3550ccae3deeed4986a15
inkwon <dlsrnjs125@naver.com>
2024-06-19 20:59:48 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -0,0 +1,13 @@
+def solution(dots):
+    answer = 0
+    [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots
+    
+    answer1 = ((y1-y2)*(x3-x4) == (y3-y4)*(x1-x2))
+    answer2 = ((y1-y3)*(x2-x4) == (y2-y4)*(x1-x3))
+    answer3 = ((y1-y4)*(x2-x3) == (y2-y3)*(x1-x4))
+    
+    if answer1 == 1 or answer2 == 1 or answer3 == 1:
+        answer = 1
+    else:
+        answer = 0
+    return answer
