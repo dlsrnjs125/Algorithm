0b6786ea957464035b7b3f7843a4fb753da3b48e
inkwon <dlsrnjs125@naver.com>
2024-07-07 23:33:14 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -0,0 +1,8 @@
+def solution(num, total):
+    answer = []
+    var = sum(range(num+1))
+    diff = total - var
+    start_num = diff//num
+    
+    answer = [i+1+start_num for i in range(num)]
+    return answer
