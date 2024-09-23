53b81185aa52c4eab3b7f5c4b89d6705ec1ba0b9
inkwon <dlsrnjs125@naver.com>
2024-09-24 00:19:37 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = []
+    
+    answer += num_list
+    
+    if num_list[-1] > num_list[-2]:
+        answer.append(num_list[-1] - num_list[-2])
+    else:
+        answer.append(num_list[-1] * 2)
+        
+    return answer
