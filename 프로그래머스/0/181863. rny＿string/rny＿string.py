a1ff79b19f2f20c36445b12766142eb34432f5d2
inkwon <dlsrnjs125@naver.com>
2024-10-18 22:57:02 +0900

    [level 0] Title: rny_string, Time: 0.02 ms, Memory: 9.92 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.py
@@ -0,0 +1,9 @@
+def solution(rny_string):
+    answer = ''
+    
+    for i in rny_string:
+        if i == 'm':
+            answer += 'rn'
+        else:
+            answer += i
+    return answer
