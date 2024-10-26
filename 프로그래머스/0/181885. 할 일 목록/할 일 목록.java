61aa86fcd9c19d7e0f1083785631c044aef1ebd8
inkwon <dlsrnjs125@naver.com>
2024-10-26 23:57:34 +0900

    [level 0] Title: 할 일 목록, Time: 0.08 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> answer = new ArrayList<>();
+        
+        for(int i=0; i<todo_list.length; i++) {
+        	if( finished[i] == false ) {
+        		answer.add(todo_list[i]);
+        	}
+        }
+        
+        return answer.toArray(new String[answer.size()]);
+    }
+}
