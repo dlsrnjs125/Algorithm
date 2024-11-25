68b2710bad16ef4ae35a30833ac1ea89450d25da
inkwon <dlsrnjs125@naver.com>
2024-11-26 05:34:18 +0900

    [level 0] Title: 세 개의 구분자, Time: 9.19 ms, Memory: 23.4 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.py
@@ -3,7 +3,7 @@ def solution(myStr):
     
     answer = myStr.replace('a',' ').replace('b',' ').replace('c',' ')
     answer = answer.split()
-    
+
     if not answer:
         answer = ["EMPTY"]
     
