3fdbd72b3e2bde725f89c604216a2b8399833903
inkwon <dlsrnjs125@naver.com>
2025-03-10 22:35:08 +0900

    [level 0] Title: 배열 만들기 2, Time: 665.45 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -0,0 +1,7 @@
+def solution(l, r):
+    answer = []
+    for num in range(l, r + 1):
+        if not set(str(num)) - set(['0', '5']):
+            answer.append(num)
+            
+    return answer if answer else [-1]
