e515d02433e2291323d4e3a5e9d93cbf60625e0e
inkwon <dlsrnjs125@naver.com>
2025-05-23 22:52:15 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,10 @@
+def solution(s):
+    answer = ''
+    
+    s = s.split(' ')
+    
+    for i in range(len(s)):
+        s[i] = s[i].capitalize()
+        answer = ' '.join(s)
+        
+    return answer
